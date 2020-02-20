6c54bac42ba050a11b4cb393002ef3714696f0d9
kervis vasquez <kervisvasquez24@gmail.com>
2020-02-20 11:08:34 -0800

    agregamos modelos controladores y migraciones a nuestro proyecto con -r

---
+++ app/Buyer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+namespace User;
+
+class Buyer extends User
+{
+    //
+}
---
+++ app/Seller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+namespace User;
+class Seller extends User
+{
+    //
+}
